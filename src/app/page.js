0f75401bf3488c6737ca1d223af0103881cdd3ae
vndv18 <vndv18@ggook.com>
2023-12-12 18:41:06 +0700

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,154 @@
-import Image from 'next/image'
+import Image from "next/image";
+
+const obj = [
+  {
+    number: "$1.80B",
+    text: "30 Day Volume",
+  },
+  {
+    number: "$0.84B",
+    text: "Managed on testX.fi",
+  },
+  {
+    number: "$21.43M",
+    text: "Total Collateral Automated",
+  },
+];
+
+const menu = ["products", "Protocols", "Tokens", "Tokens", "Use Cases"];
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div>
+      <header className="bg-white flex flex-col items-center pt-3">
+        <div className="w-full w-max-[1200] flex justify-center max-md:max-w-full pr-[11px]">
+          <div className="ml-9 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
+            <div className="flex flex-col items-stretch w-[24%] max-md:w-full max-md:ml-0">
+              <Image
+                className="aspect-[1.98] object-contain object-center overflow-hidden shrink-0 max-w-full grow max-md:mt-10"
+                src="/logo.png"
+                alt="Logo"
+                loading="lazy"
+                width={260}
+                height={135}
+              />
+            </div>
+            <nav className="flex flex-col items-stretch w-[76%] ml-[72px] max-md:w-full max-md:ml-0">
+              <div className="bg-[#F7BDCC] flex w-full items-center justify-between m-auto pl-8 pr-3.5 py-2 rounded-xl max-md:max-w-full max-md:flex-wrap max-md:mt-10 max-md:pl-5">
+                <div className="flex items-stretch justify-between gap-[80px] my-auto max-md:max-w-full max-md:flex-wrap max-md:justify-center">
+                  <a
+                    href="#"
+                    className="text-[#596F78] text-sm font-bold leading-5 whitespace-nowrap"
+                  >
+                    Products
+                  </a>
+                  <a
+                    href="#"
+                    className="text-[#596F78] text-sm font-bold leading-5"
+                  >
+                    Protocols
+                  </a>
+                  <a
+                    href="#"
+                    className="text-[#596F78] text-sm font-bold leading-5"
+                  >
+                    Tokens
+                  </a>
+                  <a
+                    href="#"
+                    className="text-[#596F78] text-sm font-bold leading-5 whitespace-nowrap"
+                  >
+                    Use Cases
+                  </a>
+                </div>
+                <div className="shadow-sm bg-white w-[230px] h-[30px] self-stretch flex items-stretch justify-center gap-1  pl-10 pr-8 pt-[5.5px] pb-[8px] ml-[92px] rounded-[32px] max-md:px-5">
+                  <div className="text-[#17344F] text-center text-base font-bold leading-6 whitespace-nowrap">
+                    Connect Wallet
+                  </div>
+                  <div className="text-blue-950 text-center text-base leading-6 self-center whitespace-nowrap my-auto">
+                    →
+                  </div>
+                </div>
+              </div>
+            </nav>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      </header>
+      <main className="main">
+        <div className="self-stretch flex w-full max-w-[1200px] mx-auto flex-col px-4 py-12 max-md:max-w-full">
+          <div className="self-center flex w-full justify-between pr-[80px] mt-12 items-start max-md:max-w-full max-md:flex-wrap max-md:mt-10">
+            <h1 className="text-white text-[69px]  leading-[80px] tracking-widest-[1.52px] grow shrink basis-auto mt-3 max-md:max-w-full max-md:text-4xl max-md:leading-[52px]">
+              Explore and Earn <br />
+              <span className="flex ">
+                <span>on</span>
+                <span className="pt-4 ml-2">
+                  <Image
+                    className=" object-contain object-center overflow-hidden "
+                    src="/logo-removebg.png"
+                    alt="Logo"
+                    loading="lazy"
+                    width={135}
+                    height={83}
+                  />
+                </span>
+              </span>
+            </h1>
+            <div className="shadow-sm bg-white w-[225px] h-[121px] flex  flex-col justify-center items-center pt-[34px] pb-3 rounded-3xl max-md:px-5">
+              <div>
+                <Image
+                  className="object-contain object-center overflow-hidden "
+                  src="/logo.png"
+                  alt="Logo"
+                  loading="lazy"
+                  width={104}
+                  height={54}
+                />
+              </div>
+              <p className="text-center text-[#8795AF] text-[12px] leading-6">
+                test Front end interview 1
+              </p>
+            </div>
+          </div>
+          <div className="bg-[#F2F5FA] flex w-[423px] flex-col justify-end mt-11 pr-[9px] py-1.5 rounded-[32px] self-start items-end max-md:mt-10 max-md:pl-5">
+            <a
+              href="#"
+              className="text-[#17344F] text-center text-base leading-[22px] whitespace-nowrap shadow-sm bg-rose-300 justify-center items-stretch px-8 py-2 rounded-[32px] max-md:px-5"
+            >
+              Sign up
+            </a>
+          </div>
+          <div className="flex max-w-full gap-[42px] items-center justify-between mt-5 self-start max-md:ml-2.5">
+            <a
+              href="#"
+              className="text-white text-center text-lg font-bold leading-[37.8px] bg-[#7BB8F1]  w-[174px] h-[40px]  rounded-[32px] max-md:px-5"
+            >
+              Log in
+            </a>
+            <button className="text-blue-950 text-center text-base font-bold leading-6 shadow-sm bg-white w-[203px] h-[40px] rounded-[32px] max-md:px-5">
+              Launch App
+            </button>
+          </div>
+          <div className="border bg-white bg-opacity-50 self-stretch flex flex-col justify-center items-center mt-[85px] mb-7 px-16 py-12 rounded-3xl border-solid border-white max-md:max-w-full max-md:mt-10 max-md:px-5">
+            <div className="flex w-full max-w-[941px] justify-between gap-5 items-start max-md:max-w-full-md:flex-wrap max-md:justify-center">
+              {obj.map((item, idx) => {
+                return (
+                  <div
+                    key={idx}
+                    className="flex grow basis-[0%] flex-col items-stretch"
+                  >
+                    <div className="text-[#17344F] text-center text-[49px] leading-[64px] max-md:text-4xl max-md:leading-[54px]">
+                      {item.number}
+                    </div>
+                    <div className="text-[#596F78] text-center text-base leading-6 whitespace-nowrap mt-[13px]">
+                      {item.text}
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
 }
